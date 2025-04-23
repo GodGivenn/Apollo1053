fbec99c5557f54ae40a3d07b7fba1a6e8250613c
GodGivenn <balicabogdaniulian@gmail.com>
2025-04-23 19:43:04 +0300

    Test Commit

--- src/ro/ase/csie/main/Main.java
+++ src/ro/ase/csie/main/Main.java
@@ -66,7 +66,7 @@ public class Main {
 //            e.printStackTrace();
 //        }
 
-
+        System.out.println(landers.getFirst());
         //write to txt file
         try {
             FileOutputStream fileOutputStream = new FileOutputStream("landed_landers.txt");
